621296fb1c6fc3af0e4817df17018b9848e22b33
elia-b <elia.boscaini@gmail.com>
2025-03-11 00:10:10 +0100

    Remove personal command

--- .config/bash/prompt.sh
+++ .config/bash/prompt.sh
@@ -40,7 +40,7 @@ home=( $HOME )
 new_prompt=""
 
 if [ "$(whoami)" == "container" ]; then
-    new_prompt+="$BLACK$BG_RED  Dev " 
+    new_prompt+="$BLACK$BG_RED 󰡨 $DEV_CONTAINER_NAME " 
 fi
 
 path_parts=(${path//\// })
