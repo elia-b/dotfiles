dea7c0efa12016748350df9ccf29bd84c935acbe
elia-b <elia.boscaini@gmail.com>
2025-12-02 23:23:33 +0100

    Add y as alias

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -65,6 +65,15 @@ extract() {
 alias tx="tmux_new_or_attach"
 alias mergepdfs="gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -sOutputFile=merged.pdf"
 
+# Direct directory change with yazi
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
+
 # ===============================
 # Personal
 # ===============================
